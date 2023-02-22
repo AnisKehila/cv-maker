fdbea476ad4b0be689382b82e56c8dbf4235582d
aniskehila <aniskehila22@gmail.com>
2023-02-22 18:41:54 +0100

    set envirenmont

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function header() {
+    return (
+        <h1 className="header">
+            Make Your Proffessional CV Online
+        </h1>
+    );
+}
+
+export default header;
